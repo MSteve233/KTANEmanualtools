86b82d30065cac7d6c2d4f4984473b17306219a0
MSte2 <MSte2@DESKTOP-J20G0MD>
2024-09-24 20:14:52 +0800

    添加项目文件。

---
+++ KTANEmanualtools/Main.cpp
@@ -0,0 +1,13 @@
+/*项目已重启
+* KTANEManualTools
+* by M_Steve
+* GitHub:https://github.com/MSteve233/KTANEManualTools/
+*/
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+int main(){
+	cout << "这里什么也没有" << endl;
+}
